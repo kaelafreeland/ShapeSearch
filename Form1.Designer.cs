e502396c1e07b966e226eb7c5b22affc418c512c
kaela <118493786+kaelafreeland@users.noreply.github.com>
2024-06-18 12:55:58 +1200

    Added basic buttons in place for start screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,19 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lblShapeSearch = new Label();
+            btnStart = new Button();
+            btnHowToPlay = new Button();
+            btnLeaderboard = new Button();
             SuspendLayout();
+            // 
+            // lblShapeSearch
+            // 
+            lblShapeSearch.AutoSize = true;
+            lblShapeSearch.Font = new Font("Microsoft Sans Serif", 50F, FontStyle.Bold, GraphicsUnit.Point);
+            lblShapeSearch.ForeColor = Color.SteelBlue;
+            lblShapeSearch.Location = new Point(300, 152);
+            lblShapeSearch.Name = "lblShapeSearch";
+            lblShapeSearch.Size = new Size(844, 113);
+            lblShapeSearch.TabIndex = 0;
+            lblShapeSearch.Text = "SHAPE SEARCH";
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.FromArgb(255, 209, 71);
+            btnStart.Font = new Font("Microsoft Sans Serif", 90F, FontStyle.Bold, GraphicsUnit.Point);
+            btnStart.Location = new Point(341, 326);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(768, 223);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "START";
+            btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnHowToPlay
+            // 
+            btnHowToPlay.BackColor = Color.YellowGreen;
+            btnHowToPlay.Font = new Font("Microsoft Sans Serif", 45F, FontStyle.Bold, GraphicsUnit.Point);
+            btnHowToPlay.Location = new Point(341, 555);
+            btnHowToPlay.Name = "btnHowToPlay";
+            btnHowToPlay.Size = new Size(768, 112);
+            btnHowToPlay.TabIndex = 2;
+            btnHowToPlay.Text = "HOW TO PLAY";
+            btnHowToPlay.UseVisualStyleBackColor = false;
+            // 
+            // btnLeaderboard
+            // 
+            btnLeaderboard.BackColor = Color.YellowGreen;
+            btnLeaderboard.Font = new Font("Microsoft Sans Serif", 45F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLeaderboard.ForeColor = Color.Black;
+            btnLeaderboard.Location = new Point(341, 673);
+            btnLeaderboard.Name = "btnLeaderboard";
+            btnLeaderboard.Size = new Size(768, 121);
+            btnLeaderboard.TabIndex = 3;
+            btnLeaderboard.Text = "LEADERBOARD";
+            btnLeaderboard.UseVisualStyleBackColor = false;
             // 
             // StartScreen
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(10F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(1478, 844);
+            Controls.Add(btnLeaderboard);
+            Controls.Add(btnHowToPlay);
+            Controls.Add(btnStart);
+            Controls.Add(lblShapeSearch);
+            Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "StartScreen";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "StartScreen";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblShapeSearch;
+        private Button btnStart;
+        private Button btnHowToPlay;
+        private Button btnLeaderboard;
     }
 }
