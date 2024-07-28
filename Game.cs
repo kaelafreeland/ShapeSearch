7e7f47c665a267f150a2e9db4e15e48aec2600f0
kaela <118493786+kaelafreeland@users.noreply.github.com>
2024-07-28 20:08:10 +1200

    Images now show up need to make them clickable

--- Game.cs
+++ Game.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ShapeSearch_kf
 {
     public partial class frmGame : Form
     {
+        private PictureBox[] pictureBoxes;
+
         public frmGame()
         {
             InitializeComponent();
             int score = 0;
-
+            InitializePbxShapes();
             RandomizeImages();
-
-           
         }
 
         private void frmGame_Load(object sender, EventArgs e)
@@ -28,65 +24,100 @@ namespace ShapeSearch_kf
             
         }
 
+        private void InitializePbxShapes()
+        {
+            pictureBoxes = new PictureBox[10];
+
+            pictureBoxes[0] = pbxShape1;
+            pictureBoxes[1] = pbxShape2;
+            pictureBoxes[2] = pbxShape3;
+            pictureBoxes[3] = pbxShape4;
+            pictureBoxes[4] = pbxShape5;
+            pictureBoxes[5] = pbxShape6;
+            pictureBoxes[6] = pbxShape7;
+            pictureBoxes[7] = pbxShape8;
+            pictureBoxes[8] = pbxShape9;
+            pictureBoxes[9] = pbxShape10;
+        }
+
         private void RandomizeImages()
         {
-            string filePath =  @"C:\Users\kaela\OneDrive - Bayfield High School\2024\comp sci\programming\shape search game\ShapeSearch_kf\shapes";
+            string filePath = @"C:\Users\kaela\OneDrive - Bayfield High School\2024\comp sci\programming\shape search game\ShapeSearch_kf\shapes";
 
-            //sorts files named in the format colour_shape into an array called images
-            string[] images = Directory.GetFiles(filePath, "*_*.png*");
+            //sort files named in the format colour_shape into an array called images
+            string[] images = Directory.GetFiles(filePath, "*_*.png");
 
-            //method to shuffle the images array
+
+            //method to shuffle array of images
             ShuffleImages(images);
 
             //gets 10 images at random from all images and creates a list
             List<string> chosenImages = images.Take(10).ToList();
 
-
-            
             //for the 10 images added to the list
             for (int i = 0; i < 10; i++)
             {
-                
+                //if pictureboxes are not empty
+                if (pictureBoxes[i] != null)
+                {
+                    //put 1 of 10 images into empty picturebox until all are full
+                    pictureBoxes[i].Image = Image.FromFile(chosenImages[i]);
+                    
+                }
             }
-
         }
-        private void ShuffleImages (string[] images)
+
+        private void ShuffleImages(string[] images)
         {
-            //random image
+            // Random image
             Random random = new Random();
+
+
+            //loops through images array from end to start
+            for (int i = images.Length - 1; i > 0; i--)
+            {
+                //gets index from 0 to current index
+                int r = random.Next(0, i + 1);
+                
+                //swap images from current to new index (r)
+                string placeholder = images[i];
+                //moves random image to index and current image to random index
+                images[i] = images[r];
+                images[r] = placeholder;
+            }
         }
 
         public void timerTime_Tick(object sender, EventArgs e)
         {
-            //convert current timer value to an int
+            // Convert current timer value to an int
             int timer = Convert.ToInt32(lblTime.Text);
-            //increment timer value by -1
+            // Increment timer value by -1
             timer = timer - 1;
-            //convert timer value to string and write to label
+            // Convert timer value to string and write to label
             lblTime.Text = Convert.ToString(timer);
             if (timer == 0)
             {
                 this.Hide();
-                //creates and opens game screen and makes form variable to address throughout code
+                // Creates and opens game screen and makes form variable to address throughout code
                 frmEnd frmEnd = new frmEnd();
-                //opens game form
+                // Opens game form
                 frmEnd.Show();
             }
         }
 
         private void pbxShape4_Click(object sender, EventArgs e)
         {
-
+           
         }
 
         private void pbxShape5_Click(object sender, EventArgs e)
         {
-
+          
         }
 
         private void pbxShape9_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
